a24f60a54a38cc4550697372c6708966c83ef6d0
Andres <142335765+andreUmag@users.noreply.github.com>
2024-11-15 18:39:20 -0500

    ORDER, PROTUCT, USER, APIGATEWAY, DISCOVERYSERVICE agregados

---
+++ UserModule/src/test/java/app/usermodule/UserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package app.usermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
